770b06f60f07188a420ccceec2fa8edbdf10f4a3
semitrivial <alexander@math.ohio-state.edu>
2015-07-07 08:13:00 +0100

    Replace gibberish test string with example string from Wikipedia

--- test.c
+++ test.c
@@ -3,12 +3,16 @@
 
 int main(void)
 {
-  char *test = "a,b,\"c,d\",\"e,f,\"\",g\",h";
+  /*
+   * Test string adapted from https://en.wikipedia.org/wiki/Comma-separated_values
+   */
+  char *test = "1996,Jeep,\"Venture \"\"Extended Edition\"\"\",\"MUST SELL!\nair, moon roof, loaded\",4799.00";
   char **parsed = parse_csv( test );
   char **ptr;
+  int i = 1;
 
   for ( ptr = parsed; *ptr; ptr++ )
-    printf( "%s\n", *ptr );
+    printf( "Field %d: %s\n", i++, *ptr );
 
   free_csv_line( parsed );
 
