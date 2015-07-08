e8722f0384bf7a9899b07d1c58e5d8e03e12acec
semitrivial <alexander@math.ohio-state.edu>
2015-07-08 11:31:47 +0100

    Make test string in test.c constant

--- test.c
+++ test.c
@@ -6,7 +6,7 @@ int main(void)
   /*
    * Test string adapted from https://en.wikipedia.org/wiki/Comma-separated_values
    */
-  char *test = "1996,Jeep,\"Venture \"\"Extended Edition\"\"\",\"MUST SELL!\nair, moon roof, loaded\",4799.00";
+  const char *test = "1996,Jeep,\"Venture \"\"Extended Edition\"\"\",\"MUST SELL!\nair, moon roof, loaded\",4799.00";
   char **parsed = parse_csv( test );
   char **ptr;
   int i = 1;
