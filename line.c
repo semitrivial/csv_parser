d2162d3b173d5cd794d8001718d244d8517893cf
semitrivial <alexander@math.ohio-state.edu>
2016-12-06 20:18:34 -0500

    Add line.c, with initial draft of split_on_unescaped_newlines

---
+++ line.c
@@ -0,0 +1,79 @@
+#include <string.h>
+#include "csv.h"
+
+/*
+ * Given a string which might contain unescaped newlines, split it up into
+ * lines which do not contain unescaped newlines, returned as a
+ * NULL-terminated array of malloc'd strings.
+ */
+char **split_on_unescaped_newlines(const char *txt) {
+    const char *ptr, *lineStart;
+    char **buf, **bptr;
+    int fQuote, nLines;
+
+    /* First pass: count how many lines we will need */
+    for ( nLines = 1, ptr = txt, fQuote = 0; *ptr; ptr++ ) {
+        if ( fQuote ) {
+            if ( *ptr == '\"' ) {
+                if ( ptr[1] == '\"' ) {
+                    ptr++;
+                    continue;
+                }
+                fQuote = 0;
+            }
+        } else if ( *ptr == '\"' ) {
+            fQuote = 1;
+        } else if ( *ptr == '\n' ) {
+            nLines++;
+        }
+    }
+
+    buf = malloc( sizeof(char*) * (nLines+1) );
+
+    if ( !buf ) {
+        return NULL;
+    }
+
+    /* Second pass: populate results */
+    lineStart = txt;
+    for ( bptr = buf, ptr = txt, fQuote = 0; ; ptr++ ) {
+        if ( fQuote ) {
+            if ( *ptr == '\"' ) {
+                if ( ptr[1] == '\"' ) {
+                    ptr++;
+                    continue;
+                }
+                fQuote = 0;
+                continue;
+            } else if ( *ptr ) {
+                continue;
+            }
+        }
+
+        if ( *ptr == '\"' ) {
+            fQuote = 1;
+        } else if ( *ptr == '\n' || !*ptr ) {
+            size_t len = ptr - lineStart + 1;
+            *bptr = malloc( len + 1 );
+
+            if ( !*bptr ) {
+                for ( bptr--; bptr >= buf; bptr-- ) {
+                    free( *bptr );
+                }
+                free( buf );
+                return NULL;
+            }
+
+            if ( *ptr ) {
+                memcpy( *bptr, lineStart, len );
+                (*bptr)[len] = '\0';
+                lineStart = ptr + 1;
+                bptr++;
+            } else {
+                memcpy( *bptr, lineStart, len + 1 );
+                bptr[1] = NULL;
+                return buf;
+            }
+        }
+    }
+}
