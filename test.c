f6302c270fbf00d3b7d99f8cd9192ff75d6c7f15
semitrivial <alexander@math.ohio-state.edu>
2016-12-06 21:51:20 -0500

    Make test.c slightly less ad-hoc

--- test.c
+++ test.c
@@ -2,34 +2,56 @@
 #include <string.h>
 #include "csv.h"
 
-void test_parse_csv(void);
-void test_split_on_unescaped_newlines(void);
+int test_parse_csv(void);
+int test_split_on_unescaped_newlines(void);
 
-int main(void) {
-  test_parse_csv();
-
-  test_split_on_unescaped_newlines();
+void run_test(const char *name, int test(void)) {
+  int result;
+  printf( "Running %s...", name );
+  result = test();
+  if (result) {
+    printf( "Success.\n" );
+  } else {
+    printf( "Failure!\n" );
+  }
 }
 
-void test_parse_csv(void)
+int main(void) {
+  run_test("test_parse_csv", test_parse_csv);
+
+  run_test("test_split_on_unescaped_newlines", test_split_on_unescaped_newlines);
+}
+
+int test_parse_csv(void)
 {
   /*
    * Test string adapted from https://en.wikipedia.org/wiki/Comma-separated_values
    */
   const char *test = "1996,Jeep,\"Venture \"\"Extended Edition\"\"\",\"MUST SELL!\nair, moon roof, loaded\",4799.00";
   char **parsed = parse_csv( test );
-  char **ptr;
-  int i = 1;
 
-  for ( ptr = parsed; *ptr; ptr++ )
-    printf( "Field %d: %s\n", i++, *ptr );
+  if ( !parsed[0] || !parsed[1] || !parsed[2] || !parsed[3] || !parsed[4] ) {
+    return 0;
+  }
+
+  if ( parsed[5] ) {
+    return 0;
+  }
+
+  if ( strcmp(parsed[0], "1996")
+  ||   strcmp(parsed[1], "Jeep")
+  ||   strcmp(parsed[2], "Venture \"Extended Edition\"")
+  ||   strcmp(parsed[3], "MUST SELL!\nair, moon roof, loaded")
+  ||   strcmp(parsed[4], "4799.00" ) ) {
+    return 0;
+  }
 
   free_csv_line( parsed );
 
-  return;
+  return 1;
 }
 
-void test_split_on_unescaped_newlines(void) {
+int test_split_on_unescaped_newlines(void) {
   const char *txt =
     "Blah, blah, blah\n"
     "Foo, \"Bar\", \"Foo, Bar\"\n"
@@ -40,7 +62,7 @@ void test_split_on_unescaped_newlines(void) {
 
   if ( !result[0] || !result[1] || !result[2] || !result[3] || result[4] ) {
     printf( "test_split_on_unescaped_newlines failed! (1)\n" );
-    return;
+    return 0;
   }
 
   if ( strcmp(result[0], "Blah, blah, blah")
@@ -48,6 +70,8 @@ void test_split_on_unescaped_newlines(void) {
   ||   strcmp(result[2], "Foo, \"B\na\nr\", Baz")
   ||   strcmp(result[3], "Foo, \"B\"\"ar\", Baz") ) {
     printf( "test_split_on_unescaped_newlines failed! (2)\n" );
-    return;
+    return 0;
   }
+
+  return 1;
 }
