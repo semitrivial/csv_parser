086f403564dd760a194d442c5fd673ad96cffc92
semitrivial <alexander@math.ohio-state.edu>
2016-12-06 19:00:02 -0500

    Add QUICK_GETC macro

--- csv.h
+++ csv.h
@@ -2,4 +2,21 @@
 #define CSV_DOT_H_INCLUDE_GUARD
 char **parse_csv( const char *line );
 void free_csv_line( char **parsed );
+
+#define READ_BLOCK_SIZE 65536
+
+#define QUICK_GETC( ch, fp )\
+do\
+{\
+    if ( read_ptr == read_end )\
+    {\
+        fread_len = fread( read_buf, sizeof(char), READ_BLOCK_SIZE, fp );\
+        if ( fread_len < READ_BLOCK_SIZE )\
+            read_buf[fread_len] = '\0';\
+        read_ptr = read_buf;\
+    }\
+    ch = *read_ptr++;\
+}\
+while(0)
+
 #endif
